642ff30da0db24bc89047ed54dfafd84b7af70e4
Gowtham K M <gowthamk007m@gmail.com>
2025-01-05 23:12:12 +0530

    shift 1

--- leetcode.py
+++ leetcode.py
@@ -775,8 +775,11 @@ shifts = [[4,3],[1,1],[0,2]]
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         call_back={1:"a",2:"b",3:"c",4:"d",5:"e",6:"f",7:"g",8:"h",9:"i",10:"j",11:"k",12:"l",13:"m",14:"n",15:"o",16:"p",17:"q",18:"r",19:"s",20:"t",21:"u",22:"v",23:"w",24:"x",25:"y",26:"z"}
-        c=call_back[2-1]
-        print(c)
+
+        for i in s:
+            print(i)
+
+        
 
 
 obj=Solution()
