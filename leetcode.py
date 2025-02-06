0589d68f2765b0d572ee8634c620885c44c8a06f
Gowtham K M <gowthamk007m@gmail.com>
2025-02-06 12:00:00 +0530

    removeOccurrences2

--- leetcode.py
+++ leetcode.py
@@ -933,7 +933,7 @@ part="abc"
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
         while part in s:
-            s = s.replace(part, "", 1)  # Remove only the first occurrence of `part`
+            s = s.replace(part, "", 1)  
         return s
 
 obj=Solution()
